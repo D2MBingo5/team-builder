64d0fa939e24abf6dfe72003c3071ddc9cd75fa6
Leu Family <romanianfobjr@gmail.com>
2021-07-20 14:38:31 -0500

    form and members list initialized

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: '',
+}
 
 function App() {
+  const [ members, setMembers ] = useState([])
+  const [ formValues, setFormValues ] = useState(initialFormValues)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Test
       </header>
+      <div className='team-member-container'>
+
+      </div>
     </div>
   );
 }
